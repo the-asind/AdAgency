a7d546448db10f2b257c80f551147fc08bc6e819
the-asind <askerkashaev@gmail.com>
2024-11-26 04:55:45 +0700

    Add migrations to configure audit log timestamp and update mockups

---
+++ Migrations/20241125210249_UpdateSeedDataWithUtcDateTime.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdAgency.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeedDataWithUtcDateTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Billboards",
+                keyColumn: "BillboardId",
+                keyValue: 4,
+                column: "LocationDescription",
+                value: "У входа в парк");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Billboards",
+                keyColumn: "BillboardId",
+                keyValue: 4,
+                column: "LocationDescription",
+                value: "У входа в п��рк");
+        }
+    }
+}
